c7d865e43f8d1327e2c888ffdc0349d360f9ef7b
Tim Lucas <t@toolmantim.com>
2017-02-06 13:15:09 +1100

    Add missing newlines

--- print-review.sh
+++ print-review.sh
@@ -17,25 +17,25 @@ COFFEECUP
 echo -en '\033[0m'
 
 echo "+++ :coffee: Roaster"
-buildkite-agent meta-data get "roaster"
+buildkite-agent meta-data get "roaster"; echo
 
 echo "+++ :coffee: Batch"
-buildkite-agent meta-data get "batch"
+buildkite-agent meta-data get "batch"; echo
 
 echo "+++ :coffee: Roast Date"
-buildkite-agent meta-data get "roast-date"
+buildkite-agent meta-data get "roast-date"; echo
 
 echo "+++ :coffee: Grams"
-buildkite-agent meta-data get "grams"
+buildkite-agent meta-data get "grams"; echo
 
 echo "+++ :coffee: Grind Setting"
-buildkite-agent meta-data get "grind-setting"
+buildkite-agent meta-data get "grind-setting"; echo
 
 echo "+++ :coffee: Pot Number"
-buildkite-agent meta-data get "pot-number"
+buildkite-agent meta-data get "pot-number"; echo
 
 echo "+++ :coffee: Rating"
-buildkite-agent meta-data get "rating"
+buildkite-agent meta-data get "rating"; echo
 
 echo "+++ :coffee: Tasting Notes"
-buildkite-agent meta-data get "tasting-notes"
+buildkite-agent meta-data get "tasting-notes"; echo
