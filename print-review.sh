ab87f9c24752384f657393be8cac90575eec71c8
Tim Lucas <t@toolmantim.com>
2017-02-06 12:59:20 +1100

    Tweak pipeline emojis and wording

--- print-review.sh
+++ print-review.sh
@@ -2,35 +2,35 @@
 
 set -euo pipefail
 
-echo "+++ :tongue::printer: Review"
+echo "+++ :tongue: :printer: Review"
 
 echo "Roaster:"
 buildkite-agent meta-data get "roaster"
-echo
+echo; echo
 
 echo "Batch:"
 buildkite-agent meta-data get "batch"
-echo
+echo; echo
 
 echo "Roast Date:"
 buildkite-agent meta-data get "roast-date"
-echo
+echo; echo
 
 echo "Grams:"
 buildkite-agent meta-data get "grams"
-echo
+echo; echo
 
 echo "Grind Setting:"
 buildkite-agent meta-data get "grind-setting"
-echo
+echo; echo
 
 echo "Pot Number:"
 buildkite-agent meta-data get "pot-number"
-echo
+echo; echo
 
 echo "Rating:"
 buildkite-agent meta-data get "rating"
-echo
+echo; echo
 
 echo "Tasting Notes:"
 buildkite-agent meta-data get "tasting-notes"
